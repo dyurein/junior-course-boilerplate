27df3147557047b228badd75d8a018d45abbfd2e
Sergei <epcijer@gmail.com>
2020-01-15 10:22:13 +0300

    array fix 2

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ function App() {
         <div className="goods">
             <h1 className="goods__title">Список товаров</h1>
             <ul className="goods__list">
-            {data.map((link)=> 
+            {data.slice(0, 3).map((link)=> 
                 <li className="goods__item" key={link.id}>
                     <a className="goods__item-link" href="#">
                         {link.name}
                     </a>
                 </li>
-            ).slice(0, 3)}
+            )}
             </ul>
         </div>
     )
